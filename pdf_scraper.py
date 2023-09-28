bf18956a06a9612fbf62640d1f9767914d0b25ad
LazloSteele <lazlosteele@gmail.com>
2023-09-27 20:10:23 -0600

    broke it to hopefully fix it

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -20,48 +20,17 @@ class PDF_Scraper(object):
         return path
     
     @staticmethod
-    def stage_pdf(pdf, event_type):
-        if event_type not in PDF_Scraper.valid_event_types:
-            raise ValueError(f'Event type must be one of the following: {valid_event_types}')
+    def stage_pdf(pdf):
         
         text = extract_text(pdf)
-        contents = list(filter(lambda x: x != '', text.split('\n')))
 
         #initialize values for event
         event_number = ''
         event_name = ''
         skaters = []
 
-        if event_type == 'IJS':
-            
-            '''
-            removes extreneous data that is inconsistent in the data ordering in the pdf sources
-            '''  
-            to_remove = ['Starting', 'Number', 'Name', 'Nation']
-            for n in to_remove:
-                contents.remove(n)
-            
-            '''
-            name and number of event has always been third element in the pdf after stripping, happy to find better way
-            '''
-            event_num_and_name = contents[2].strip().upper()
-
-            number_of_skaters = 0
-            for n in contents:
-                try:
-                    int(n)
-                    number_of_skaters += 1
-                except:
-                    pass
-
-            #wtf is this string indexing for? Clean this up.    
-            skaters = [n for n in contents[(3+number_of_skaters):-3]]
-            skaters = skaters[::2]
-
-            
+        '''    
         elif event_type == '6.0':
-            
-            event_num_and_name = contents[1].strip().upper()
 
             # in 6.0 pdf some skaters have a newline character between their skating order # and their name
             # how to regex the list of strings that contents is and also catch the instance of extra newline
@@ -84,14 +53,15 @@ class PDF_Scraper(object):
                     skaters.append(this_skater)
                 
                 n+=1
-
+        '''
         '''
         pulls any instance of digits and an optional alpha within parenthesis
         positive lookahead and lookbehind to not include the parenthesis
         meant to capture the event number
         '''
-        event_number = re.findall(r'(?<=\()[0-9]+[a-z]?(?=\))', event_num_and_name)[0]
-        event_name = re.findall(r'(?<=\)\s)[a-zA-Z0-9\s\\\/\-]+', event_num_and_name)[0]
+
+        event_number = re.findall(r'(?<=\n\()[0-9]+[a-z]?(?=\))', text)[0].strip().upper()
+        event_name = re.findall(r'(?<=\)\s)[a-zA-Z0-9( )\\\/\-]+', text)[0].strip().upper()
         if not event_number:
             raise ValueError('PDF does not contain expected event number format: \'(001) abc\' -OR- \'(001a) abc\'')
 
@@ -102,6 +72,30 @@ class PDF_Scraper(object):
             }
 
         return(event)
+
+    @staticmethod
+    def handle_ijs(content):
+        '''
+        removes extreneous data that is inconsistent in the data ordering in the pdf sources
+        '''  
+        #to_remove = ['Starting', 'Number', 'Name', 'Nation']
+        #for n in to_remove:
+        #    contents.remove(n)
+
+        '''
+        number_of_skaters = 0
+        for n in contents:
+            try:
+                int(n)
+                number_of_skaters += 1
+            except:
+                pass
+
+        #wtf is this string indexing for? Clean this up.    
+        skaters = [n for n in contents[(3+number_of_skaters):-3]]
+        skaters = skaters[::2]
+        '''
+        pass
 
     @staticmethod
     def bulk_stage_pdf(directory):
@@ -119,4 +113,8 @@ class PDF_Scraper(object):
 if __name__ == '__main__':
     data = PDF_Scraper.stage_pdf(PDF_Scraper.get_file_path(), '6.0')
     print(data)
+
+    data = PDF_Scraper.stage_pdf(PDF_Scraper.get_file_path(), 'IJS')
+    print(data)
+
 
