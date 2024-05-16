68aa5f552815c02fc38db79ef24a25dbefb2246e
Lazlo Steele <lazlosteele@gmail.com>
2024-05-15 21:20:08 -0600

    finished affirmative test for event model

--- tests/test_event.py
+++ tests/test_event.py
@@ -25,6 +25,6 @@ class TestEvent:
         chacha_slide = self.session.query(Event).filter_by(
             event_number="205b").first()
 
-        assert chacha_slide.competition_id == 1
+        assert chacha_slide.competition_id == 2
         assert chacha_slide.event_name != "Enver Invitational"
         assert chacha_slide.event_number == "205b"
