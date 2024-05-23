7c124260d878967c093a91ab1b229e350f20942c
Lazlo Steele <lazlosteele@gmail.com>
2024-05-22 19:34:28 -0600

    Everything in jinja2 template!

--- Skatertron/main.py
+++ Skatertron/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from routers import competition, event, skate, file
 
 
@@ -7,8 +10,18 @@ app.include_router(competition.router)
 app.include_router(event.router)
 app.include_router(skate.router)
 app.include_router(file.router)
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def root():
-    return {"Hello": "Skaters"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request},
+    )
