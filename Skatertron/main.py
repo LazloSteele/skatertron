4c46f3f9123aff187ef1c3a56bcb0a44a5396c82
Lazlo Steele <lazlosteele@gmail.com>
2024-06-25 21:30:06 -0600

    started commenting because I probably should

--- Skatertron/main.py
+++ Skatertron/main.py
@@ -4,22 +4,27 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from routers import competition, event, skate, file
 
+# this is the app
 app = FastAPI(title="Skatertron", docs_url="/api/docs")
+
+# include fastapi routers to the data model endpoints
 app.include_router(competition.router)
 app.include_router(event.router)
 app.include_router(skate.router)
 app.include_router(file.router)
+
+# Mount a directory for serving static files like CSS and JavaScript
 app.mount(
     "/static",
     StaticFiles(directory="static"),
     name="static"
 )
 
+# initialize html templating engine
 templates = Jinja2Templates(directory="templates")
 
-current_competition = 0
 
-
+# root path for main app frame
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
 
