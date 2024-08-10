1ec0220566b5ff4f04d248b0a159046fe93e3a16
Lazlo Steele <lazlosteele@gmail.com>
2024-08-10 14:47:32 -0600

    Messaging queue mq_worker and mq_task modules listen and respond appropriately

--- Skatertron/mq/mq_task.py
+++ Skatertron/mq/mq_task.py
@@ -1,8 +1,27 @@
+import pika
 import sys
 
 
-message_body = ' '.join(sys.argv[1:]) or "Test Message!"
-channel.basic_publish(exchange='',
-                      routing_key='mq_messages',
-                      body=message_body)
-print(f" [x] Sent {message_body}")
+class MQTask:
+
+    @staticmethod
+    def new_task(message = "Test!"):
+        with pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) as connection:
+            channel = connection.channel()
+
+            channel.queue_declare(queue='task_queue', durable=True)
+
+            message_body = message
+            channel.basic_publish(
+                exchange='',
+                routing_key='task_queue',
+                body=message_body,
+                properties=pika.BasicProperties(
+                    delivery_mode=pika.DeliveryMode.Persistent
+                ))
+            print(f" [x] Sent {message_body}")
+            connection.close()
+
+
+if __name__ == "__main__":
+    MQTask.new_task(' '.join(sys.argv[1:]))
--- Skatertron/mq/mq_worker.py
+++ Skatertron/mq/mq_worker.py
@@ -1,22 +1,29 @@
 import pika
 import time
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+class MQWorker:
+    def __init__(self, connection):
+        self.connection = connection
+        self.channel = self.connection.channel()
+
+        self.channel.queue_declare(queue='task_queue', durable=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue='task_queue', on_message_callback=self.callback)
+
+        self.channel.start_consuming()
+
+    @staticmethod
+    def callback(self, ch, method, properties, body):
+        print(f" [x] Received {body.decode()}")
+        time.sleep(body.count(b'.'))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-def callback(ch, method, properties, body):
-    print(f" [x] Received {body.decode()}")
-    time.sleep(body.count(b'.'))
-    print(" [x] Done")
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+if __name__ == "__main__":
+    with pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) as connection:
+        worker = MQWorker(connection)
 
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
-
-channel.start_consuming()
