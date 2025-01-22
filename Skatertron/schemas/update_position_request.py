c5618976e149044ce4c0fc12da9d5fce0e997a72
Lazlo Steele <lazlosteele@gmail.com>
2025-01-21 22:10:52 -0700

    the js is working, and the schema is up-to-date. next step is updating the api router

--- Skatertron/schemas/update_position_request.py
+++ Skatertron/schemas/update_position_request.py
@@ -2,6 +2,4 @@ from pydantic import BaseModel, Field
 
 
 class UpdateEventPositionRequest(BaseModel):
-    event_id: int = Field(..., description="The ID of the event, represented as a string")
-    event_position: int = Field(..., description="The current position of the event")
-    new_event_position: int = Field(..., description="The new position to move the event to")
+    event_id_list: list[int] = Field(..., description="The db ID of the event, represented as a list of ints")
